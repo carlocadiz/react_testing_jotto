7e37d7edc7d35e937a5df08e8d02a9e872b49509
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-24 23:40:50 -0400

    added propTypes for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 /**
@@ -22,6 +23,10 @@ const Congrats =  (props) => {
         <div data-test='component-congrats' />
     );
    }
+}
+
+Congrats.propTypes = {
+  success : PropTypes.bool.isRequired,
 }
 
 export default Congrats;
