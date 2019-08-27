ea4d08f626cb35163f581217858dfb7812795c67
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-27 16:19:00 -0400

    set up guessWord reducer and integration tests

---
+++ src/reducers/guessedWordReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function guessWordReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - Action to be reduced.
+ * @returns {array} - new guessWords state.
+*/
+
+
+export default (state, action) => {
+  return null;
+}
